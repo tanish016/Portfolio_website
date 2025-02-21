7039da31297d00bc0e0ce1bd4caca604dc13dd52
Tanish <tanishtp01@gmail.com>
2025-02-21 21:03:04 +0530

    Second commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+     <h1 className="text-3xl font-bold underline text-center pt-10 hover:text-blue-500">
+    Hello world!
+  </h1>
+     </div>
+    </>
+  )
+}
+
+export default App
